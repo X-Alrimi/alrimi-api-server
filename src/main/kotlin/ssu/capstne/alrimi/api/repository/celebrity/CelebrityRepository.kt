ceb5c727d70054f686a997f938585ad45ff30ed1
ktj1997 <rlaxowns7916@gmail.com>
2021-04-10 23:42:37 +0900

    fix:Celebrity Query 수정

--- src/main/kotlin/ssu/capstne/alrimi/api/repository/celebrity/CelebrityRepository.kt
+++ src/main/kotlin/ssu/capstne/alrimi/api/repository/celebrity/CelebrityRepository.kt
@@ -6,6 +6,6 @@ import ssu.capstne.alrimi.api.model.Celebrity
 import ssu.capstne.alrimi.api.repository.celebrity.projection.CelebrityInfoTransfer
 
 interface CelebrityRepository : JpaRepository<Celebrity, Long> {
-    @Query("SELECT c From Celebrity c WHERE c.company =:company AND parent_id IS NULL")
+    @Query("SELECT c From Celebrity c WHERE c.company.id =:companyId AND parent_id IS NULL")
     fun findAllByCompany_IdAndParentIdIsNull(companyId: Long): List<CelebrityInfoTransfer>
 }
