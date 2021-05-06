6207ed35f9d40b49f2d44c14e96a6d7219eeae78
ktj1997 <rlaxowns7916@gmail.com>
2021-05-06 18:30:20 +0900

    feat/NgramSearch + NewsSave

--- src/main/kotlin/com/ssu/capstone/alrimi/api/service/api/ApiServiceImpl.kt
+++ src/main/kotlin/com/ssu/capstone/alrimi/api/service/api/ApiServiceImpl.kt
@@ -5,29 +5,32 @@ import com.ssu.capstone.alrimi.api.controller.dtos.news.NewsDto
 import com.ssu.capstone.alrimi.api.service.celebrity.CelebrityService
 import com.ssu.capstone.alrimi.api.service.company.CompanyService
 import com.ssu.capstone.alrimi.api.service.news.NewsService
-import com.ssu.capstone.alrimi.core.util.SearchUtil
+import com.ssu.capstone.alrimi.core.util.CustomNgramAnalyzer
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
 class ApiServiceImpl(
-    private val companyService: CompanyService,
-    private val celebrityService: CelebrityService,
-    private val newsService: NewsService
+        private val companyService: CompanyService,
+        private val celebrityService: CelebrityService,
+        private val newsService: NewsService
 ) : ApiService {
     override fun findKeyword(crawledData: NewsCrawlerDto): MutableList<NewsDto> {
-        var result: MutableList<NewsDto> = mutableListOf()
+        val result: MutableList<NewsDto> = mutableListOf()
         val companies = companyService.getSimpleCompanyList()
 
         for (news in crawledData.news) {
+            val nGramList = CustomNgramAnalyzer.getNgram(news.text)
             var flag = false
             for (company in companies) {
                 val celebrities = celebrityService.getCelebritiesList(company.id)
                 for (celebrity in celebrities) {
-                    if (SearchUtil.findKeyword(news.text, celebrity.name)) {
-                        newsService.save(news, company, celebrity)
-                        flag = true
+                    nGramList.forEach { keyword ->
+                        if (keyword == celebrity.name) {
+                            newsService.save(news, company, celebrity)
+                            flag = true
+                        }
                     }
                 }
             }
--- src/main/kotlin/com/ssu/capstone/alrimi/core/util/CustomNgramAnalyzer.kt
+++ src/main/kotlin/com/ssu/capstone/alrimi/core/util/CustomNgramAnalyzer.kt
@@ -1,10 +1,12 @@
 package com.ssu.capstone.alrimi.core.util
 
 import org.apache.lucene.analysis.Analyzer
+import org.apache.lucene.analysis.TokenStream
 import org.apache.lucene.analysis.ko.KoreanAnalyzer
 import org.apache.lucene.analysis.ko.KoreanPartOfSpeechStopFilter
 import org.apache.lucene.analysis.ko.KoreanTokenizer
 import org.apache.lucene.analysis.ko.dict.UserDictionary
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute
 
 object CustomNgramAnalyzer {
     private var analyzer: Analyzer? = null
@@ -16,11 +18,18 @@ object CustomNgramAnalyzer {
                 false)
     }
 
-    fun getInstance(): Analyzer {
-        if (this.analyzer == null) {
-            analyzer = KoreanAnalyzer()
-
+    fun getNgram(text: String): List<String> {
+        val list: MutableList<String> = mutableListOf()
+        val tokenStream: TokenStream = analyzer!!.tokenStream("text", text)
+        val tokenAttr: CharTermAttribute = tokenStream.addAttribute(CharTermAttribute::class.java)
+        try {
+            tokenStream.reset()
+            while (tokenStream.incrementToken())
+                list.add(tokenAttr.toString())
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-        return analyzer!!
+        tokenStream.close()
+        return list
     }
 }
