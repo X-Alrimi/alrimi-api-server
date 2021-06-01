f337d35b4390748eed480ddd6758264f1ed5ce02
ktj1997 <rlaxowns7916@gmail.com>
2021-06-01 16:26:55 +0900

    fix/page

--- src/main/kotlin/com/ssu/capstone/alrimi/api/repository/PageUtil.kt
+++ src/main/kotlin/com/ssu/capstone/alrimi/api/repository/PageUtil.kt
@@ -3,5 +3,5 @@ package com.ssu.capstone.alrimi.api.repository
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 
-class PageUtil(page: Int) : PageRequest(page, 4, Sort.by("id").descending()) {
+class PageUtil(page: Int) : PageRequest(page, 10, Sort.by("id").descending()) {
 }
